bf9c3ff804f59353dd4e5cb7be8c0f11515074cd
Ahmed Elfiky <aelfiky@beyond-creation.net>
2020-10-16 07:53:33 +0200

    linebreak rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "eslint linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"],
+    "linebreak-style": 0
   },
   overrides: [
     {
